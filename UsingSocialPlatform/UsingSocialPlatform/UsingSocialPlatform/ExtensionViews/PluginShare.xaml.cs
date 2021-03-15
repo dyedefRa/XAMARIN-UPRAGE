e6fb3f98f0d21d148bf4b7c6cd066e9f150f82d7
Baki OZTURK <22.exe.94@gmail.com>
2021-03-15 22:29:31 +0200

    2021-03-15 22:30

---
+++ UsingSocialPlatform/UsingSocialPlatform/UsingSocialPlatform/ExtensionViews/PluginShare.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Plugin.Share;
+using Plugin.Share.Abstractions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UsingSocialPlatform.ExtensionViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PluginShare : ContentPage
+    {
+        public PluginShare()
+        {
+            InitializeComponent();
+            this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            string title = txtTitle.Text;
+            string message = txtMessage.Text;
+            string url = txtUrl.Text;
+            if (string.IsNullOrEmpty(url))
+                url = "https://www.google.com";
+
+            ShareMessage shareMessage = new ShareMessage()
+            {
+                Text = message,
+                Title = title,
+                Url = url
+            };
+            //CrossShare.Current.Share(shareMessage);
+            CrossShare.Current.Share(shareMessage);
+
+        }
+    }
+}
