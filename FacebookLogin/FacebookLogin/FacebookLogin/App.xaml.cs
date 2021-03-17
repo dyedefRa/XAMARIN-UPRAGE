16642b60e68e1b90278059cb0f43bf41a6e2c7c0
Baki OZTURK <22.exe.94@gmail.com>
2021-03-17 20:23:09 +0200

    2021-03-17 20:22

---
+++ FacebookLogin/FacebookLogin/FacebookLogin/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using FacebookLogin.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FacebookLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new FacebookPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FacebookLogin/FacebookLogin/FacebookLogin/Views/FacebookPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FacebookLogin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FacebookPage : ContentPage
+    {
+        //string url = "https://www.facebook.com/dialog/oauth?client_id=496961464650517&redirect_uri=https://www.facebook.com/connect/login_success.html";
+
+        private string FacebookClientId = "496961464650517";
+        private string FacebookStatic = "https://www.facebook.com/connect/login_success.html";
+
+        public FacebookPage()
+        {
+            InitializeComponent();
+            this.BackgroundColor = Color.White;
+
+            string apiRequest = "https://www.facebook.com/dialog/oauth?client_id="
+               + FacebookClientId
+               + "&display=popup&response_type=token&redirect_uri="
+               + FacebookStatic;
+
+            var webView = new WebView
+            {
+                Source = apiRequest,
+                HeightRequest = 1
+            };
+            Content = webView;
+        }
+    }
+}
