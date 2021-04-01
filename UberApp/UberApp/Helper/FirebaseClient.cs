b484c13f7a6ee209ea3dc2a19d3cd65c803b39c8
Baki OZTURK <22.exe.94@gmail.com>
2021-04-01 17:52:53 +0300

    2021-04-01

---
+++ UberApp/UberApp/Helper/FirebaseClient.cs
@@ -0,0 +1,66 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Firebase;
+using Firebase.Database;
+
+namespace UberApp.Helper
+{
+    public class FirebaseClient
+    {
+       
+
+        public static void InitializeDatabase(Context context)
+        {
+            FirebaseDatabase database;
+            var app = FirebaseApp.InitializeApp(context);
+            if (app == null)
+            {
+                var options = new FirebaseOptions.Builder()
+                    .SetApplicationId("uberapp-56725")
+                    .SetApiKey("AIzaSyAmZzmZD9EXFvqruksdNSHPYH6VV1e6uxw")
+                     .SetDatabaseUrl("https://uberapp-56725-default-rtdb.firebaseio.com/")
+                    .SetStorageBucket("uberapp-56725.appspot.com")
+                    .Build();
+
+                app = FirebaseApp.InitializeApp(context, options);
+            }
+            database = FirebaseDatabase.GetInstance(app);
+
+
+
+            DatabaseReference dbref = database.GetReference("UserSupport");
+            dbref.SetValue("Ticket1");
+
+            Toast.MakeText(context, "Completed", ToastLength.Short).Show();
+        }
+
+        public static FirebaseDatabase GetDatabase()
+        {
+            var app = FirebaseApp.InitializeApp(Application.Context);
+            FirebaseDatabase database;
+
+            if (app == null)
+            {
+                var option = new FirebaseOptions.Builder()
+                   .SetApplicationId("uberapp-56725")
+                   .SetApiKey("AIzaSyAmZzmZD9EXFvqruksdNSHPYH6VV1e6uxw")
+                   .SetDatabaseUrl("https://uberapp-56725-default-rtdb.firebaseio.com/")
+                   .SetStorageBucket("uberapp-56725.appspot.com")
+                   .Build();
+                app = FirebaseApp.InitializeApp(Application.Context, option);
+
+            }
+            database = FirebaseDatabase.GetInstance(app);
+
+            return database;
+        }
+    }
+}
---
+++ UberApp/UberApp/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using UberApp.Helper;
+
+namespace UberApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnTestConnection;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btnTestConnection = (Button)FindViewById(Resource.Id.myFirstButton);
+            btnTestConnection.Click += BtnTestConnection_Click;
+        }
+
+        private void BtnTestConnection_Click(object sender, System.EventArgs e)
+        {
+            FirebaseClient.GetDatabase();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
